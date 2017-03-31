45da271c72a3d06be030887c7cd093c76de7e0b1
linahoyos <lina.taborda@utp.edu.co>
2017-03-31 17:49:34 -0500

    correccion im

--- Procesador/InstructionMemory.vhd
+++ Procesador/InstructionMemory.vhd
@@ -11,18 +11,33 @@ entity InstructionMemory is
 end InstructionMemory;
 
 architecture Behavioral of InstructionMemory is
+type rom_type is array (0 to 63) of std_logic_vector (31 downto 0);
 
+		
+	impure function InitRomFromFile (RomFileName : in string) return rom_type is
+		FILE RomFile : text open read_mode is RomFileName;
+		variable RomFileLine : line;
+		variable temp_bv : bit_vector(31 downto 0);
+		variable temp_mem : rom_type;
+		begin
+			for I in rom_type'range loop
+				readline (RomFile, RomFileLine);
+				read(RomFileLine, temp_bv);
+				temp_mem(i) := to_stdlogicvector(temp_bv);
+			end loop;
+		return temp_mem;
+	end function;
+	
+signal instructions : rom_type := InitRomFromFile("testJMPL.data");
 
-type memory is array(63 downto 0) of std_logic_vector(31 downto 0);
-signal dato : memory:=(others => "00000000000000000000000000000000");
-
+	
 begin
 	process(rst,Direccion)
 	begin
 		if rst='1' then
-			dato<=(others => "00000000000000000000000000000000");
+			instructions<=(others => "00000000000000000000000000000000");
 			else
-				salida<= dato(conv_integer(Direccion));
+				salida<= instructions(conv_integer(Direccion));
 			
 		end if;
 	end process;

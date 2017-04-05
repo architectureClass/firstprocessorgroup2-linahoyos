635e46779787429da69577338080a63ab4269dec
linahoyos <lina.taborda@utp.edu.co>
2017-04-04 19:24:35 -0500

    TB_ALu-ALu

--- Procesador/alu.vhd
+++ Procesador/alu.vhd
@@ -19,6 +19,9 @@
 ----------------------------------------------------------------------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+use ieee.std_logic_arith.all;
+use IEEE.NUMERIC_STD.ALL;
+use IEEE.STD_LOGIC_SIGNED.ALL;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
@@ -33,34 +36,37 @@ entity alu is
     Port ( suma1 : in  STD_LOGIC_VECTOR (31 downto 0);
            suma2 : in  STD_LOGIC_VECTOR (31 downto 0);
            alu_op : in  STD_LOGIC_VECTOR (5 downto 0);
-           salida_alu : in  STD_LOGIC_VECTOR (31 downto 0));
+           salida_alu : out  STD_LOGIC_VECTOR (31 downto 0));
 end alu;
 
 architecture Behavioral of alu is
 
-begin
 
-
-process (suma1,suma2,alu_op)
+signal result : std_logic_vector (31 downto 0) := x"00000000";
 begin 
-
-case (alu_op) is
-                   when "000001" => -- add 
-						      salida_alu <= suma1 + suma2;
-                   when "000010" => -- adcc
-						      salida_alu <= suma1 + suma2;
-								
-								
-
-
-				salida_alu <= suma1 or suma2;				
-				
-			when others => --nop
-				salida_ALU <= (others=>'0');
+process (suma1,suma2,alu_op)
+begin
+		case (alu_op) is
+                  when "000000" => -- add 
+								result <= suma1 + suma2;
+                  when "000001" =>--AND
+								result <= suma1 and suma2;
+						when "000010" =>--OR
+								result <= suma1 or suma2;
+						when "000011" =>
+								result <= suma1 xor suma2;
+						when "000100" =>
+								result <= suma1 xnor suma2;
+						when  "000101"=>
+								result<=suma1-suma2;
+						
+					
+				    	when others => --nop
+				          result<= x"00000000";
 	
 				end case;	
 	  end process; 
-			
+	 salida_alu <= result;	
 				
 end Behavioral;
 

f86d1c88e17c695ef1c06b7697780ff5f8ee6b48
linahoyos <lina.taborda@utp.edu.co>
2017-04-29 01:30:55 -0500

    modificacion PSR

--- ProcesadorCuatro/PSR.vhd
+++ ProcesadorCuatro/PSR.vhd
@@ -19,16 +19,17 @@ signal  cwp_Salida : STD_LOGIC_VECTOR (4 downto 0) := "00000";
 signal  c_Salida : STD_LOGIC :='0';
 
 begin
-
-	process(NZVC,rts,clk,nCwp)
+process(NZVC,rts,clk)
 	begin
 			if(rts = '1') then
 				cwp_Salida  <="00000";
 				c_Salida <= '0';
-			end if;
-			if(rising_edge(clk)) then
-			  cwp_Salida <= nCwp;
-			  c_Salida <= NZVC(0);
+			
+			else
+				if(rising_edge(clk)) then
+				cwp_Salida <= nCwp;
+				c_Salida <= NZVC(0);
+				end if;
 			end if;
 	end process;
 	
